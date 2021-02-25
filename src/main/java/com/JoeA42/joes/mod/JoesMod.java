9047f94ffcda0f3f29e12a9572626448068cccce
Joe_A <joe.alvarez42@gmail.com>
2021-02-25 11:28:51 -0600

    [First Block & Creative tabs] Completed a first version of the block, and added creative tabs to the game, also corrected a bug that prevented names from showing

--- src/main/java/com/JoeA42/joes/mod/JoesMod.java
+++ src/main/java/com/JoeA42/joes/mod/JoesMod.java
@@ -1,14 +1,53 @@
 package com.JoeA42.joes.mod;
 
+import com.JoeA42.joes.mod.registry.ModBlocks;
 import com.JoeA42.joes.mod.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import org.lwjgl.system.CallbackI;
 
+/**
+ * This is the main class from where the mod is launched from
+ */
 public class JoesMod implements ModInitializer {
 
+    /**
+     * This is the mod ID used as an identifier throughout the mod
+     */
     public static final String MOD_ID = "joesmod";
 
+    /**
+     * Main item group of the mod, it will be showed as a creative tab
+     */
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+            new Identifier(MOD_ID, "general"),
+            () -> new ItemStack(ModItems.RUBY));
+
+    /**
+     * This is an example of a secondary item group, with specific items
+     */
+    public static final ItemGroup OTHER_GROUP = FabricItemGroupBuilder.create(
+            new Identifier(MOD_ID, "other"))
+            .icon(() -> new ItemStack(Blocks.ENCHANTING_TABLE))
+            .appendItems(stacks -> {
+                stacks.add(new ItemStack(ModBlocks.RUBY_BLOCK));
+                stacks.add(new ItemStack(Items.APPLE));
+                stacks.add(new ItemStack(Items.GOLDEN_LEGGINGS));
+            })
+            .build();
+
+    /**
+     * Main method that initializes the mod
+     */
     @Override
     public void onInitialize() {
         ModItems.registerItems();
+        ModBlocks.registerBlocks();
     }
 }
--- src/main/java/com/JoeA42/joes/mod/registry/ModItems.java
+++ src/main/java/com/JoeA42/joes/mod/registry/ModItems.java
@@ -1,17 +1,28 @@
 package com.JoeA42.joes.mod.registry;
 
 import com.JoeA42.joes.mod.JoesMod;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-
+/**
+ * This class is used to create and register items
+ */
 public class ModItems {
 
-    public static final Item RUBY = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    //Items
+    public static final Item RUBY = new Item(new Item.Settings().group(JoesMod.ITEM_GROUP));
 
+    //Block Items
+    public static final BlockItem RUBY_BLOCK = new BlockItem(ModBlocks.RUBY_BLOCK, new Item.Settings().group(JoesMod.ITEM_GROUP));
+
+
+    /**
+     * This method is in charge of adding items to the register
+     */
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(JoesMod.MOD_ID, "ruby"), RUBY);
+        Registry.register(Registry.ITEM, new Identifier(JoesMod.MOD_ID, "ruby_block"), RUBY_BLOCK);
     }
 }
